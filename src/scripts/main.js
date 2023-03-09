eee6a7aed570441a1588d7146eaa52bea85e279d
Ting <woodming9889@gmail.com>
2023-03-10 01:19:12 +0800

    add annotation to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,7 +31,7 @@ for(let i = 0;i < cardbody.length;i++){
 };
 
 
-/* 購物車刪除按鈕 */
+/* 購物車刪除 */
 tbody.addEventListener("click",(e)=>{
     if (e.target.nodeName === "BUTTON") {
         e.target.parentElement.parentElement.remove();
@@ -42,13 +42,13 @@ tbody.addEventListener("click",(e)=>{
 });
 
 
-/* 清空購物車按鈕 */
+/* 清空購物車 */
 cleancartbtn.addEventListener("click",(e)=>{
     tbody.textContent = "";
     totalPriceSpan.textContent = "$0";  // 按下清空購物車按鈕時，總價欄位也歸零
     if (e.target.nodeName === "I") {
         tbody.textContent = "";
-        totalPriceSpan.textContent = "$0"; // 按下清空購物車按鈕時，總價欄位也
+        totalPriceSpan.textContent = "$0"; // 按下清空購物車按鈕時，總價欄位也歸零
     }
 });
 
@@ -58,18 +58,18 @@ function updatePrice() {
     const quantityInputs = document.querySelectorAll(".quantity");
     let singlePrice = document.querySelectorAll(".p"); // 單價
     const subtotalPrice = document.querySelectorAll(".sum"); // 小計
-    let totalPrice = 0;
+    let totalPrice = 0; // 宣告總價起始值為 0
     for (let i = 0; i < quantityInputs.length; i++) {
         const quantity = Number(quantityInputs[i].value);
         const price = Number((singlePrice[i].textContent).slice(1));
-        let sum = quantity * price;  // update the sum td with the new calculated sum
+        let sum = quantity * price;  // 小計金額為數量 * 單價
  
         quantityInputs[i].addEventListener("input", (e)=>{
-            subtotalPrice[i].textContent = "$" + Number(e.target.value) * sum;
-            totalPrice += sum;  // add the new sum to the total price
-            totalPriceSpan.textContent = "$" + totalPrice.toFixed(2); // update the total price span with the new calculated total price
+            subtotalPrice[i].textContent = "$" + Number(e.target.value) * sum; //對應的input更新內容每按下一次，小計也會更新
+            totalPrice += sum;  // 把對應input欄位的總價金額更新(type為Number)
+            totalPriceSpan.textContent = "$" + totalPrice.toFixed(2); // 更新對應input欄位的總價的金額顯示(type為String)
         })
-        totalPrice += sum; // 每個貓咪的小計加總
+        totalPrice += sum; // 把購物車內的所有個小貓小計加總成為購物車總價
     }
-    totalPriceSpan.textContent = "$" + totalPrice.toFixed(2);
+    totalPriceSpan.textContent = "$" + totalPrice.toFixed(2); // 總價的金額顯示(type為String)
 };
